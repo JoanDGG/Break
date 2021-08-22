77b33407394845143bcf923a19b779c272b03d13
JoanDGG <ggjoan2000@gmail.com>
2021-08-22 12:27:43 -0500

    Menu prueba1

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey && transform.GetChild(0).gameObject.activeSelf)
+        {
+            transform.GetChild(0).GetComponent<FadeOut>().Efecto();
+        }
+    }
+}
