f8eaa2a057ced2743f986dbbb89b4e8541663cac
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-08-22 13:59:43 -0700

    Pruebas de movimiento en TEST

--- Assets/Scripts/MovimientoGeneral.cs
+++ Assets/Scripts/MovimientoGeneral.cs
@@ -23,15 +23,56 @@ public class MovimientoGeneral : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movHorizontal = Input.GetAxis("Horizontal");
-        movVertical = Input.GetAxis("Vertical");
+        movVertical = 0.0f;
+        movHorizontal = 0.0f;
+        string name = this.gameObject.name;
+        switch (name)
+        {
+            case "Bob":
+                if (Input.GetKey("w"))
+                {
+                    movVertical = 0.5f;
+                }
+                if (Input.GetKey("a"))
+                {
+                    movHorizontal = -0.5f;
+                }
+                if(Input.GetKey("s"))
+                {
+                    movVertical = -0.5f;
+                }
+                if (Input.GetKey("d"))
+                {
+                    movHorizontal = 0.5f;
+                }
+                    break;
+            case "Jerry":
+                if (Input.GetKey("up"))
+                {
+                    movVertical = 0.5f;
+                }
+                if (Input.GetKey("left"))
+                {
+                    movHorizontal = -0.5f;
+                }
+                if (Input.GetKey("down"))
+                {
+                    movVertical = -0.5f;
+                }
+                if (Input.GetKey("right"))
+                {
+                    movHorizontal = 0.5f;
+                }
+                break;
+        }
+       
         Vector2 move = new Vector2(movHorizontal, movVertical);
 
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
+        //if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        //{
+           // lookDirection.Set(move.x, move.y);
+           // lookDirection.Normalize();
+        //}
 
         //anim.SetFloat("Look X", lookDirection.x);
         //anim.SetFloat("Look Y", lookDirection.y);
